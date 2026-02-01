9a5e156665810e80fb09268ab7595b2e41670175
Mit Gajera <gajeramit180@gmail.com>
2026-02-02 00:15:19 +0530

    fix: convert submodules to regular directories and clean up gitignore

---
+++ anchor_escrow/programs/anchor_escrow/src/lib.rs
@@ -0,0 +1,28 @@
+use anchor_lang::prelude::*;
+
+pub mod instructions;
+pub mod state;
+
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("J7twts5HggPGzKhtU2syNCYQfL4ZzdHfvNwe92oM7qqU");
+
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.vault_transfer()?;
+        ctx.accounts.maker_transfer()?;
+        ctx.accounts.close_vault()
+    }
+}
